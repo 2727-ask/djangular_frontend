fb2dd112ba3d19157546217672cea27ef6567f6a
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-05-07 17:03:21 +0530

    switching

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 
